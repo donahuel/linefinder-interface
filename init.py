44712bf91294741d0072f82fc9e343ba720226ba
donahuel <66244100+donahuel@users.noreply.github.com>
2021-03-02 12:09:13 -0600

    update init.py to work with fscan

--- init.py
+++ init.py
@@ -11,37 +11,47 @@ def main():
 
     run = 'O2'
     #IMPORTANT: Put 'rootdir' as the data folder within this program, or else the database will not populate.
-    rootdir = '/Users/Scraf/Downloads/test_program_normalized/test_program_normalized/data'
+    rootdir = 'C:/Users/Scraf/Downloads/LFInterface/linefinder-interface-main/data'
     file_list = []
     for subdir, dirs, files in os.walk(rootdir):
         for file in files:
-            if file.endswith('.mat'):
+            if file.endswith('.txt'):
                 filepath = os.path.join(subdir, file)
                 file_list.append(filepath)
                 
-    
+    sig_lines = []
     for file in file_list:
-        week = file.split('/')[6].split('_')[2]
+        week = file.split('\\')[2].split('_')[1] + "-" + file.split('\\')[2].split('_')[2] + "-" + file.split('\\')[2].split('_')[3]
 
-        channel = file.split('/')[6].replace('_DQ_data.mat','').split('\\')[2]
+        channel = file.split('\\')[3]
+        numsiglines = len(sig_lines)
 
-        data = loadmat(file)
-        freqs = list(data.get('freqs').flatten())
-        cohs = list(data.get('coh').flatten())
-        print(len(freqs),len(cohs))
-        sig_lines = []
-        threshold = 0.95
-        for coh in cohs:
-            if coh>threshold and coh<1:
-                sig_freq = freqs[cohs.index(coh)]
-                sig_lines.append((sig_freq, coh))
+        print("Currently working with file: " + file.split('\\')[4] + " for channel " + channel + " in week " + week + "...")
 
-        for sig_line in sig_lines:
-            freq, coh = sig_line
-            line=Line(freq=freq, coh=coh, week=week, run=run, channel=channel)
-            db.session.add(line)
+        data = open(file, "r")
+        for line in data:
+            currline = data.readline().split(' ') #Splits read line into frequency (index 0) and associated coherence (index 1)
+            if (currline[0].split('e')[1] == "+00"): #If the frequency isn't altered by the scientific notation after it... (i.e. 5*10^0)
+                freq = float(currline[0].split('e')[0]) #Splits frequency XXXXe+00 into XXXX and e+00, then stores XXXX as frequency
+            else: #Else...
+                freq = float(currline[0].split('e')[0]) * (10 ** float(currline[0].split('e')[1]))
+            freq = np.round(freq, 6)
+
+            coh = float(currline[1]) #Storing the coherence is... simpler.
+
+            threshold = 0.15 #Threshold for coherence. Coherences below this value are not stored as significant lines (or at all)
+            if coh > threshold and coh < 1: #Eliminates coherences below the threshold and extraneous coherences above 1 (with fscan, this shouldn't be an issue)
+                sig_lines.append((freq, coh, channel, week))
+        print("Moving to next file. Found " + str(len(sig_lines) - numsiglines) + " significant lines in file. Threshold is: " + str(threshold))
+
+    print("There is no next file. Commiting " + str(len(sig_lines)) + " significant lines to the database...")
+    for sig_line in sig_lines:
+        freq, coh, channel, week = sig_line
+        line=Line(freq=freq, coh=coh, week=week, run=run, channel=channel)
+        db.session.add(line)
 
     db.session.commit()
+    print("Committed " + str(len(sig_lines)) + " significant lines to the database.")
 print("Running...")
 main()
 
