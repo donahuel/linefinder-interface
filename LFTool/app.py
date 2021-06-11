7bd5a52f11542030f4d11b709fb616664cc84c02
donahuel <66244100+donahuel@users.noreply.github.com>
2021-06-11 09:19:49 -0500

    Adding QoL change to production code
    QoL change: limiting line searches to <2500 results, cleaning up forms and documentation

--- LFTool/app.py
+++ LFTool/app.py
@@ -1,4 +1,4 @@
-#This file searches the database populated by init.py and displays the results on a webpage.
+#This file produces a web interface which searches a database populated by init.py for line objects and displays the results on a webpage.
 #Authors: Vincent He, Larry Donahue, Malachy Bloom, Michael Yang
 
 from flask import Flask, render_template, url_for, flash, request, redirect, Response
@@ -52,7 +52,6 @@ def index():
         searchForm.freqlb.data == "0"
         searchForm.cohlb.data == "0"
         
-        #Catches erroneous date format inputted by users, and throws an error
         if len(searchForm.stDate.data) != 0 and len(searchForm.stDate.data) != 10:
             return render_template('lineform.html', form=searchForm, errormessage="Error: Invalid date format.")
         if len(searchForm.endDate.data) != 0 and len(searchForm.endDate.data) != 10:
@@ -103,107 +102,109 @@ def index():
 
         stringListSortedBy = []
         id = 0    
-            
+        
         for l in lines: #For each line...
-            #Set checks for each field to false
-            rnCheck = False
-            yrCheck = False
-            monthCheck = False
-            dayCheck = False
-            obCheck = False
-            chCheck = False
-            fqCheck = False
-            coCheck = False
-            
-            if len(searchForm.endDate.data) == 0: # if End Date field is empty, there is effectively no bound on end date
-                endYear = str(9999)
-                endMonth = str(99)
-                endDay = str(99)
-            else: # slice the string from user input to find the respective year, month, and day
-                endYear = searchForm.endDate.data[:4]
-                endMonth = searchForm.endDate.data[5:7]
-                endDay = searchForm.endDate.data[8:]
-
-            if len(searchForm.stDate.data) == 0: # if Start Date field is empty, there is effectively no bound on start date
-                startYear = str(0)
-                startMonth = str(0)
-                startDay = str(0)
-            else: # slice the string from user input to find the respective year, month, and day
-                startYear = searchForm.stDate.data[:4]
-                startMonth = searchForm.stDate.data[5:7]
-                startDay = searchForm.stDate.data[8:]
-
-            # slice the week of the line object into year, month, and day components
-            year = l.week[:4]
-            month = l.week[5:7]
-            day = l.week[8:]
-
-            if searchForm.run.data in l.run: #If run matches search query
-                rnCheck = True #...pass run check.
-            if rnCheck:    
-                if request.form.get('H1') == l.obs or request.form.get('L1') == l.obs or (request.form.get('H1') == request.form.get('L1') == None):
-                    obCheck = True
-            if obCheck: #If run check is passed...
-                if (startYear < year and endYear > year): #...and year is between startYear and endYear search query...
-                    yrCheck = True #...pass year range check.
-                    monthCheck = True #...pass month range check.
-                    dayCheck = True #...pass day range check.
-                elif (startYear == year or endYear == year): #...and year is the same as either startYear or endYear search query...
-                    yrCheck = True #...pass year range check.
-            if yrCheck: #If year range check is passed...
-                if (startMonth < month and endMonth > month): #...and month range matches search query...
-                    monthCheck = True #...pass month range check.
-                    dayCheck = True
-                elif (startMonth == month or endMonth == month):
-                    monthCheck = True
-            if monthCheck: #If month range check is passed...
-                if (startDay <= day and endDay >= day): #...and day range matches search query...
-                    dayCheck = True #...pass day range check.
-            if dayCheck: #If day range check is passed...
-                if searchForm.channel.data in l.channel or searchForm.channel.data.upper() in l.channel or len(searchForm.channel.data) == 0: #...and channel matches search query OR channel field is empty...
-                    chCheck = True
-            if chCheck: #If channel (and run, week) checks are passed...
-                if len(searchForm.frequb.data) == 0 and len(searchForm.freqlb.data) != 0: #...we see if the search query frequency has a lower, but no upper, bound. If so...
-                    if float(searchForm.freqlb.data) < l.freq: #...we check if the line has a greater frequency than the lower bound...
-                        fqCheck = True #...if so, pass frequency check.
-                elif len(searchForm.freqlb.data) == 0 and len(searchForm.frequb.data) != 0: #...we see if the search query frequency has an upper, but no lower, bound. If so...
-                    if float(searchForm.frequb.data) > l.freq: #...we check if the line has a lesser frequency than the upper bound...
-                        fqCheck = True #...if so, pass frequency check.
-                elif len(searchForm.freqlb.data) != 0 and len(searchForm.frequb.data) != 0: #...we see if the search query is bounded on both ends. If so...
-                    if float(searchForm.freqlb.data) < l.freq and float(searchForm.frequb.data) > l.freq: #...we check if the line's frequency is within bounds...
-                        fqCheck = True #...if so, pass frequency check.
-                else: #...by the first 3 mutually exclusive statements passing, this means both frequency queries are blank. And thus...
-                    fqCheck = True #...the frequency check passes.
-            if fqCheck: #If frequency (and run, week, channel) checks are passed...
-                if len(searchForm.cohub.data) == 0 and len(searchForm.cohlb.data) != 0: #...we see if the search query coherence has a lower, but no upper, bound. If so...
-                    if float(searchForm.cohlb.data) < l.coh: #...we check if the line has a greater coherence than the lower bound...
-                        coCheck = True #...if so, pass coherence check.
-                elif len(searchForm.cohlb.data) == 0 and len(searchForm.cohub.data) != 0: #...we see if the search query coherence has an upper, but no lower, bound. If so...
-                    if float(searchForm.cohub.data) > l.coh: #...we check if the line has a lesser coherence than the upper bound...
-                        coCheck = True #...if so, pass coherence check.
-                elif len(searchForm.cohlb.data) != 0 and len(searchForm.cohub.data) != 0: #...we see if the search query is bounded on both ends. If so...
-                    if float(searchForm.cohlb.data) < l.coh and float(searchForm.cohub.data) > l.coh: #...we check if the line's coherence is within bounds...
-                        coCheck = True #...if so, pass coherence check.
-                else: #...by the first 3 mutually exclusive statements passing, this means both coherence queries are blank. And thus...
-                    coCheck = True #...the coherence check passes.
-            if coCheck: #If all checks are passed...
-                dlines.append(l) #Add line to desired lines\ 
+            if len(dlines) < 2500: #Makes sure dlines does not swell too big, prevents breaking of page
+                #Set checks for each field to false
+                rnCheck = False
+                yrCheck = False
+                monthCheck = False
+                dayCheck = False
+                obCheck = False
+                chCheck = False
+                fqCheck = False
+                coCheck = False
                 
-                for lines in dlines:
-                    subList = []
-                    stringLine = str(lines)
-                    subList.append(stringLine.split(","))
+                if len(searchForm.endDate.data) == 0: # if End Date field is empty, there is effectively no bound on end date
+                    endYear = str(9999)
+                    endMonth = str(99)
+                    endDay = str(99)
+                else: # slice the string from user input to find the respective year, month, and day
+                    endYear = searchForm.endDate.data[:4]
+                    endMonth = searchForm.endDate.data[5:7]
+                    endDay = searchForm.endDate.data[8:]
 
-                for i in subList:
-                    i[4] = float(i[4])
-                    i[5] = float(i[5])
+                if len(searchForm.stDate.data) == 0: # if Start Date field is empty, there is effectively no bound on start date
+                    startYear = str(0)
+                    startMonth = str(0)
+                    startDay = str(0)
+                else: # slice the string from user input to find the respective year, month, and day
+                    startYear = searchForm.stDate.data[:4]
+                    startMonth = searchForm.stDate.data[5:7]
+                    startDay = searchForm.stDate.data[8:]
+
+                # slice the week of the line object into year, month, and day components
+                year = l.week[:4]
+                month = l.week[5:7]
+                day = l.week[8:]
+
+                if searchForm.run.data in l.run: #If run matches search query
+                    rnCheck = True #...pass run check.
+                if rnCheck:    
+                    if request.form.get('H1') == l.obs or request.form.get('L1') == l.obs or (request.form.get('H1') == request.form.get('L1') == None):
+                        obCheck = True
+                if obCheck: #If run check is passed...
+                    if (startYear < year and endYear > year): #...and year is between startYear and endYear search query...
+                        yrCheck = True #...pass year range check.
+                        monthCheck = True #...pass month range check.
+                        dayCheck = True #...pass day range check.
+                    elif (startYear == year or endYear == year): #...and year is the same as either startYear or endYear search query...
+                        yrCheck = True #...pass year range check.
+                if yrCheck: #If year range check is passed...
+                    if (startMonth < month and endMonth > month): #...and month range matches search query...
+                        monthCheck = True #...pass month range check.
+                        dayCheck = True
+                    elif (startMonth == month or endMonth == month):
+                        monthCheck = True
+                if monthCheck: #If month range check is passed...
+                    if (startDay <= day and endDay >= day): #...and day range matches search query...
+                        dayCheck = True #...pass day range check.
+                if dayCheck: #If day range check is passed...
+                    if searchForm.channel.data in l.channel or searchForm.channel.data.upper() in l.channel or len(searchForm.channel.data) == 0: #...and channel matches search query OR channel field is empty...
+                        chCheck = True
+                if chCheck: #If channel (and run, week) checks are passed...
+                    if len(searchForm.frequb.data) == 0 and len(searchForm.freqlb.data) != 0: #...we see if the search query frequency has a lower, but no upper, bound. If so...
+                        if float(searchForm.freqlb.data) < l.freq: #...we check if the line has a greater frequency than the lower bound...
+                            fqCheck = True #...if so, pass frequency check.
+                    elif len(searchForm.freqlb.data) == 0 and len(searchForm.frequb.data) != 0: #...we see if the search query frequency has an upper, but no lower, bound. If so...
+                        if float(searchForm.frequb.data) > l.freq: #...we check if the line has a lesser frequency than the upper bound...
+                            fqCheck = True #...if so, pass frequency check.
+                    elif len(searchForm.freqlb.data) != 0 and len(searchForm.frequb.data) != 0: #...we see if the search query is bounded on both ends. If so...
+                        if float(searchForm.freqlb.data) < l.freq and float(searchForm.frequb.data) > l.freq: #...we check if the line's frequency is within bounds...
+                            fqCheck = True #...if so, pass frequency check.
+                    else: #...by the first 3 mutually exclusive statements passing, this means both frequency queries are blank. And thus...
+                        fqCheck = True #...the frequency check passes.
+                if fqCheck: #If frequency (and run, week, channel) checks are passed...
+                    if len(searchForm.cohub.data) == 0 and len(searchForm.cohlb.data) != 0: #...we see if the search query coherence has a lower, but no upper, bound. If so...
+                        if float(searchForm.cohlb.data) < l.coh: #...we check if the line has a greater coherence than the lower bound...
+                            coCheck = True #...if so, pass coherence check.
+                    elif len(searchForm.cohlb.data) == 0 and len(searchForm.cohub.data) != 0: #...we see if the search query coherence has an upper, but no lower, bound. If so...
+                        if float(searchForm.cohub.data) > l.coh: #...we check if the line has a lesser coherence than the upper bound...
+                            coCheck = True #...if so, pass coherence check.
+                    elif len(searchForm.cohlb.data) != 0 and len(searchForm.cohub.data) != 0: #...we see if the search query is bounded on both ends. If so...
+                        if float(searchForm.cohlb.data) < l.coh and float(searchForm.cohub.data) > l.coh: #...we check if the line's coherence is within bounds...
+                            coCheck = True #...if so, pass coherence check.
+                    else: #...by the first 3 mutually exclusive statements passing, this means both coherence queries are blank. And thus...
+                        coCheck = True #...the coherence check passes.
+                if coCheck: #If all checks are passed...
+                    dlines.append(l) #Add line to desired lines
+                    print(len(dlines))
+                    
+                    for lines in dlines:
+                        subList = []
+                        stringLine = str(lines)
+                        subList.append(stringLine.split(","))
+
+                    for i in subList:
+                        i[4] = float(i[4])
+                        i[5] = float(i[5])
+                    
+                        sortedBy = sorted(subList, key = lambda x: x[4])
                 
-                    sortedBy = sorted(subList, key = lambda x: x[4])
-            
-            for i, lines in enumerate(sortedBy):
-                id += 1
-                newDict = {"id" : id , "run" : lines[0], "obs" : lines[1], "week" : lines[2], "channel" : lines[3], "freq" : lines[4], "coh" : lines[5]}
-                stringListSortedBy.append(newDict)
+                for i, lines in enumerate(sortedBy):
+                    id += 1
+                    newDict = {"id" : id , "run" : lines[0], "obs" : lines[1], "week" : lines[2], "channel" : lines[3], "freq" : lines[4], "coh" : lines[5]}
+                    stringListSortedBy.append(newDict)
             
         #get the value from the dropdown menu, sort by that value.
         sortMenu = request.form.get('sorting')
@@ -263,10 +264,14 @@ def index():
         global dLines
         dLines = dlines
 
-        return render_template('lineresult.html', dlines=stringListSortedBy)
+        if len(dLines) == 2500:
+            return render_template('lineresult.html', dlines=stringListSortedBy, warning="Line search limited to 2500 results. Confine bounds for a more specific search.")
+        
+        else:
+            return render_template('lineresult.html', dlines=stringListSortedBy, warning="")
 
     if request.method == 'GET':
-        return render_template('lineform.html', form=searchForm, errormessage="", helpmessage="", tries=1)
+        return render_template('lineform.html', form=searchForm, errormessage="", helpmessage="")
 
     else:
         "Something went wrong."
@@ -291,4 +296,3 @@ def getPlotCSV():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
